fc1b9adbb69d21624f5300493287f5a5098ff900
alex <sania_free@i.ua>
2016-04-15 11:21:45 +0300

    one page

---
+++ js/common.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	//fancybox
+	$('.fancybox').fancybox({
+		helpers: {
+			overlay: {
+				locked: false
+			}
+		}
+	});
+	
+	//bxslider
+	$('.video_block ul').bxSlider();
+});
